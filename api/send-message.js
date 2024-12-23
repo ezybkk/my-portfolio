f257d2a37e9925571e8786f532717efaefff2cab
ezybkk <kleinkeneth@gmail.com>
2024-12-23 14:05:13 +0800

    just updated

--- api/send-message.js
+++ api/send-message.js
@@ -1,6 +1,6 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer');  // Change to CommonJS require
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     if (req.method !== 'POST') {
         return res.status(405).json({ success: false, message: 'Method Not Allowed' });
     }
